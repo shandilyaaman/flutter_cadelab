b040313053bb5d3e3ed1565768fc9d3265c96092
Aman Shandilya <shandilya.aman@gmail.com>
2019-05-26 12:00:46 +0530

    added GridView in tabpage2

--- lib/tabview/tabpage1.dart
+++ lib/tabview/tabpage1.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
 class TabPage1 extends StatelessWidget {
+  ListView showListView(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: _getItemUi,
+    );
+  }
+
+  Widget _getItemUi(context, index) {
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Image.network(
+            'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+        new Text(
+          'I love food',
+          style: TextStyle(
+            color: Colors.white, 
+            fontSize: 22,
+            letterSpacing: 5.0),
+          textAlign: TextAlign.left,
+        ),
+        new Divider(color: Colors.blue,)
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
-      child: Center(
-        child: Text(
-          'Tab Page 1',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
+      padding: EdgeInsets.all(16.0),
+      color: Colors.black,
+      child: showListView(context),
     );
   }
 }
